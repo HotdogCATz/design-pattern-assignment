cae77b2cc880bc39c7b7e216f02f4636c186b2ed
HotdogCATz <wutichaipatthaisong@gmail.com>
2025-01-10 18:25:09 +0700

    ex1-1

--- ex1_1_abstract_factory/App.java
+++ ex1_1_abstract_factory/App.java
@@ -1,8 +1,28 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter widget type: 1.motif, 2.pm");
+        System.out.print("Enter: ");
+        String widgetType = sc.nextLine();
+        sc.close();
+        IWidgetFactory factory;
         // client use WidgetFactoryProvider
-        IWidgetFactory factory = WidgetFactoryProvider.getWidgetFactory("motif");
+        switch (widgetType) {
+            case "1" -> {
+                factory = WidgetFactoryProvider.getWidgetFactory("motif");
+                break;
+            }
 
+            case "2" -> {
+                factory = WidgetFactoryProvider.getWidgetFactory("pm");
+                break;
+            }
+
+            default -> throw new AssertionError();
+        }
         IScrollBar scrollBar = factory.createScrollBar();
         IWindow window = factory.createWindow();
         displayWidget(window, scrollBar);
